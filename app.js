0f16f8afe740ed8738873faaa8b588eb80a5dbeb
nikoloz2 <vnikoloz@gmail.com>
2024-10-21 23:43:40 +0400

    from Mars with love

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Humans!'));
+app.get('/', (req, res) => res.send('Hello Humans! We are from Mars!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
